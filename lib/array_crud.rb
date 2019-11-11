68b13954509dab958df0cbb57d2dc5e1fe8b0230
Cindy Kei <myk245@nyu.edu>
2019-11-11 00:08:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,6 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   add_element_to_end_of_array(pets, "hamster")
+    pets << "hamster"
 end
 
 def add_element_to_start_of_array(array, element)
